9c6c1d244c0458d7e5a292eec327bc7cf361ce2e
Mariana Vanucci <marianavanucciandrade@gmail.com>
2023-11-08 18:12:12 -0300

    setting structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import imagemMobile from "./data/images/illustration-woman-online-mobile.svg";
+import "./App.css";
+import Question from "./Components/Question";
+import Titulo from "./Components/Titulo";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Titulo></Titulo>
+      <Question></Question>
     </div>
   );
 }
